c0fee3c7b1bc222cb926125326f777b84946aa65
JoaqAb <joaquinabuin@gmail.com>
2023-09-13 01:37:17 -0300

    dependecies and json demo

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
     description: { type: String, required: true},
     price: { type: String, required: true},
     available: { type: Boolean, default: true},
-    img: { type: String, required: true}
+    image: { type: String, required: true}
 });
 
 const productModel = mongoose.model(collection, productSchema);
