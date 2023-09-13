8fb37944339023afd68f89e88f529b68eb71044e
JoaqAb <joaquinabuin@gmail.com>
2023-09-12 22:39:55 -0300

    ignore .env

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -9,6 +9,7 @@ const productSchema = new mongoose.Schema({
     description: { type: String, required: true},
     price: { type: String, required: true},
     available: { type: Boolean, default: true},
+    img: { type: String, required: true}
 });
 
 const productModel = mongoose.model(collection, productSchema);
